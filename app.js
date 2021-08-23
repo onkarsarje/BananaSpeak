3aad95bf96d92b7280934924b2d4096ad52c1511
onkarsarje <73869353+onkarsarje@users.noreply.github.com>
2021-08-23 17:03:22 +0530

    push

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ var btnTranslate = document.querySelector("#btn-translate");
 
 var txtInput = document.querySelector("#txt-input");
 
-var outputDiv document.querySelector("#output");
+var outputDiv = document.querySelector("#output");
 
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+  return serverURL +"?"+"text="+text
+}
+
+function errorHandler(error){
+  console.log("error Occured: ",error);
+  alert("Something wrong with the Server, Try after some time")
+}
 
 
 function clickHandler() {
-      outputDiv.innerText="Aasadasda"+ txtInput.value;
+      var inputText=txtInput.value;
+
+      fetch(getTranslationURL(inputText))
+        .then(response=>response.json())
+        .then(json=> {
+          var translatedText=json.contents.translated;
+          outputDiv.innerText=translatedText;  
+          })
+        .catch(errorHandler)
     }
 
 
